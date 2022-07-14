2d54f38087f43d90cd2cf7d2e381efec5a9625e4
xilinkix <stanislav.papulov@yandex.ru>
2022-07-14 10:33:20 +0500

    загрузил первый урок с гита

--- src/lesson1/Lesson1.java
+++ src/lesson1/Lesson1.java
@@ -1,73 +1,74 @@
 package lesson1;
 
-import java.sql.SQLOutput;
-
 public class Lesson1 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world!");
 
         //Примитивы
         //Нравится ли вам кофе
-        //true - да
-        //false - нет
+        //true - да, false - нет
         boolean answer = false;
+        boolean myAnswer = true;
         System.out.println("Нравится ли вам кофе?");
         System.out.println(answer);
 
-        //от -128 до 127
+        //От минус 128 до 128
         byte bt = -128;
 
-        //БОЛЬШЕ ЗНАЧЕНИЙ -32765 ДО 32767
-        short SH = 3200;
+        //Больше значений -32765 до 32767
+        short sh = 32_767;
 
-        //Диапазон  -2 147 483 648 до 2 147 483 649
+        //Диапазон от -2 147 483 648 до 2 147 483 649
+        int number = 32_767;
 
-        int number = -2_000_000_000;
-
-        //
+        //Диапазон до 2 в 64 степени
         long ln = 8_000_000_000L;
 
-        //Числа с плаваюзей точкой
-        float fl = 3.1234567f;
-        System.out.println(fl);
+        //Числа с плавающей точкой
+        float fl = 3.123456789012345f;
+        //Больше знаков после запятой
+        double db = 3.1234567890123456789;
 
-        double db = 3.12345678901234567890;
-        System.out.println(db);
-
+        //Мат операции
         int number1 = 10;
-        int number2 = 2;
-        int number3 = 4;
-        int result1 = number1 / number2;
-        int result2 = number1 + number2;
-        int result3 = number1 - number2;
-        int result4 = number1 * number2;
-        int result5 = number1 / number3;
-        System.out.println(result1);
+        double number2 = 3;
+        double result = number1 / number2;
+        double result2 = number1 - number2;
+        double result3 = number1 + number2;
+        double result4 = (number1 * number2) * 2;
 
         //Символы
-        char ch = 105 + 20000;
+        char ch = 105 + 1;
         System.out.println(ch);
 
-        int[]   array = new int[5];
+        //Массивы
+        //Массив чисел
+        int[] array = new int[5];
         array[0] = 10;
-        System.out.println(array[0]);
+        System.out.println(array);
 
-
-
+        //Массив символов
         char[] arrayChar = new char[5];
-        array[0] = 'в';
-        array[1] = 'о';
-        array[2] = 'д';
-        array[3] = 'к';
-        array[4] = 'а';
-        String vodka = "водка";
+        arrayChar[0] = 'в';
+        arrayChar[1] = 'о';
+        arrayChar[2] = 'д';
+        arrayChar[3] = 'к';
+        arrayChar[4] = 'а';
+        System.out.println(arrayChar);
+
+        //Короткий способ объявления массива
+        char[] newArray = new char[]{'в', 'о', 'д', 'к', 'а'};
+
+        //Текст
+        String vodka = "водка 100";
         System.out.println(vodka);
         String forChapaev = " для Чапаева";
 
-        //Конкатенация
+        //Конкатенация (соединение двух строк в одну)
+        String resultChapaev = vodka + forChapaev;
+        System.out.println(resultChapaev);
 
-
-
+        //Конкатенацию можно делать с другими типами данных, например String + double. Результат String
+        System.out.println("Информация: " + 3.12);
     }
 }
-
